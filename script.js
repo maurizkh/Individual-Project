bc7c50b0357aed946ba0f671d5ca34bc2467f6b4
maurizkh <101409110+maurizkh@users.noreply.github.com>
2022-04-01 20:52:20 +0700

    aaaaaaaa

--- script.js
+++ script.js
@@ -1,19 +1,117 @@
-function insert(num){
-	document.form.textarea.value = document.form.textarea.value + num;
+//nemerik input
+const numbers = document.querySelectorAll('.number')
+console.log(numbers)
+numbers.forEach((number) => {
+	number.addEventListener('click', (event) => {
+		inputNumber(event.target.value)
+		updateScreen(currentNumber)
+	})
+})
+
+
+const calculatorScreen = document.querySelector('.calculator-screen')
+
+const updateScreen = (number) => {
+	calculatorScreen.value = number
 }
 
-function equal(){
-	var hasil = document.form.textarea.value;
-	document.form.textarea.value = eval(hasil);
+const inputNumber = (number) => {
+	if (currentNumber === '0') {
+		currentNumber = number
+	} else {
+		currentNumber += number
+	}
 }
 
-function clean(){
-	document.form.textarea.value = "";
+//kalkulasi
+let prevNumber = ''
+let calculationOperator = ''
+let currentNumber = ''
+
+const operators = document.querySelectorAll('.operator')
+
+operators.forEach ((operator) => {
+	operator.addEventListener('click', (event) => {
+		inputOperator(event.target.value)
+	})
+})
+
+const inputOperator = (operator) => {
+	if (calculationOperator === '') {
+		prevNumber = currentNumber
+	}
+	calculationOperator = operator
+	currentNumber = ''
 }
 
-function back(){
-	var hasil = document.form.textarea.value;
-	document.form.textarea.value = hasil.substring(0,hasil.length-1);
+//percent
+const percent = document.querySelector('.percentage')
+
+percent.addEventListener('click', () => {
+	calculate()
+	updateScreen(currentNumber)
+})
+
+
+//aktifin fungsi
+const equalSign = document.querySelector('.equal-sign')
+
+equalSign.addEventListener('click', () => {
+	calculate()
+	updateScreen(currentNumber)
+})
+
+const calculate = () => {
+	let result = ''
+	switch (calculationOperator) {
+		case "+":
+			result = parseFloat(prevNumber) + parseFloat(currentNumber)
+			break
+		case "-":
+			result = prevNumber - currentNumber
+			break
+		case "*":
+			result = prevNumber * currentNumber
+			break
+		case "/":
+			result = prevNumber / currentNumber
+			break
+		case "%":
+			result = prevNumber / 100
+		default:
+			break
+	}
+	currentNumber = result
+	calculationOperator = ''
 }
 
-//yang dari tutorial progate aku gabisa bisa mas maap:'
+
+//Fungsi tombol AC
+const clearBtn = document.querySelector('.all-clear')
+
+clearBtn.addEventListener('click', () => {
+	clearAll()
+	updateScreen(currentNumber)
+})
+
+const clearAll = () => {
+	prevNumber = ''
+	calculationOperator = ''
+	currentNumber = '0'
+}
+
+//desimal
+const decimal = document.querySelector('.decimal')
+
+decimal.addEventListener('click', (event) => {
+	inputDecimal(event.target.value)
+	updateScreen(currentNumber)
+})
+
+inputDecimal = (dot) => {
+	if(currentNumber.includes('.')) {
+		return
+	}
+	currentNumber += dot
+}
+
